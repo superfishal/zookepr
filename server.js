caa2e8da10dab529604bf32f6cccc06c1e3036bd
superfishal <97133716+superfishal@users.noreply.github.com>
2022-04-11 12:43:22 -0700

    uncommitted changes not pushed

--- server.js
+++ server.js
@@ -65,7 +65,11 @@ app.get("/api/animals/:id", (req, res) => {
     res.send(404);
   }
 });
-
+app.post("/api/animals", (req, res) => {
+  // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
+});
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
